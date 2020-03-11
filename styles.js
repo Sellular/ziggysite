2d9e60277179cbde5feb3f5d3cb7902284fb576a
Sellular <sellular13@gmail.com>
2020-03-11 18:08:44 -0500

    Finalized design for Family, Images, and Blog page. Added mirrored Ziggy head in header

---
+++ styles.js
@@ -0,0 +1,3 @@
+document.write('<link rel="stylesheet" href="master.css">');
+document.write('<link rel="stylesheet" href="header.css">');
+document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">');
