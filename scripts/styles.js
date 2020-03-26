a24a60e167dbb7a8bb92c6ddc683ce6e7442dbac
Eric Sell <sellular13@gmail.com>
2020-03-26 09:32:53 -0500

    More refactoring and organizing

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,3 +1,3 @@
-document.write('<link rel="stylesheet" href="master.css">');
-document.write('<link rel="stylesheet" href="header.css">');
+document.write('<link rel="stylesheet" href="styles/master.css">');
+document.write('<link rel="stylesheet" href="styles/header.css">');
 document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">');
