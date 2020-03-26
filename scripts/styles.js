2f7ad840cb96cbdf657f92554e06aa0d58c9118e
Eric Sell <sellular13@gmail.com>
2020-03-26 10:38:31 -0500

    Start of mobile formatting

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,3 +1,5 @@
+document.write('<link rel="icon" type="image/png" href="images/Ziggy/IMG_3933crop.png">');
 document.write('<link rel="stylesheet" href="styles/master.css">');
 document.write('<link rel="stylesheet" href="styles/header.css">');
 document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">');
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1">');
